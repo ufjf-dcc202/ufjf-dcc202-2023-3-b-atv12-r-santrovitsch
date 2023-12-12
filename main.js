2e6b4ed66820f1fb52df2796a9fe4e2ee69e461b
Rafael Vicente Santrovitsch <rafael.santrovitsch@estudante.ufjf.br>
2023-12-12 00:43:24 -0300

    liguei botao de limpar lista com sua função

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ import { getEstoque, transacaoNoEstoque, limpaEstoque } from "./estoque.js";
 
 const olJoao = document.querySelector("#joao");
 const olMaria = document.querySelector("#maria");
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('buttonLimparLista').addEventListener('click', () => {
+        limpaEstoque();
+        atualizaTela();
+    });
+});
+
